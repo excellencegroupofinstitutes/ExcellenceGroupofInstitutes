cb7f65ae5939dc3d793bdf80c7c4057f7dff3718
Gurpreet-Singh-01 <gurpreet.singh.work.29@gmail.com>
2025-08-24 13:43:25 +0530

    Added color

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className='text-3xl text-amber-700'>First</div>
+    <div className='text-3xl text-primary'>
+      <button className='border-2 border-primary p-5 m-5' >Hell</button>
+      <img src="/images/logo.png" alt="" />
+    </div>
   )
 }
 
