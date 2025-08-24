ccd50a3ff0ac10fedc053ba736826c1a079ade79
Gurpreet-Singh-01 <gurpreet.singh.work.29@gmail.com>
2025-08-24 16:42:33 +0530

    Fixed navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
 import Navbar from './components/Navbar'
+import {createBrowserRouter} from 'react-router-dom'
+
+
 
 const App = () => {
   return (
     <>
     <Navbar/>
+    <div className='h-[2000px]'></div>
     </>
   )
 }
