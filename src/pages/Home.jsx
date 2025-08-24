fb6d860f40c29b5a3a6d1a8b6466c5b07c8b4223
Gurpreet-Singh-01 <gurpreet.singh.work.29@gmail.com>
2025-08-24 22:43:10 +0530

    Added core section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import HeroSection from '../components/hero/HeroSection'
+import CoreSection from '../components/hero/CoreSection'
 
 const Home = () => {
   return (
     <div className='mt-32 md:mt-39'>
         <HeroSection/>
+        <CoreSection/>
     </div>
 )
 }
