8176299cd2b461d42d237ebb81a51e76761730a5
Gurpreet-Singh-01 <gurpreet.singh.work.29@gmail.com>
2025-08-25 23:05:12 +0530

    Started Web service design

--- src/pages/WebService.jsx
+++ src/pages/WebService.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import HeroWebService from '../components/webService/HeroWebService'
+import OurServices from '../components/webService/OurServices'
 
 const WebService = () => {
   return (
     <div className='mt-45 md:mt-65'>
         <HeroWebService />
+        <OurServices />
     </div>
   )
 }
