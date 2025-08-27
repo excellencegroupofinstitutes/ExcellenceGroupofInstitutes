be841382c0cd8c44efd91c0ff9fab0279b795dde
Gurpreet-Singh-01 <gurpreet.singh.work.29@gmail.com>
2025-08-27 22:29:12 +0530

    completed basic web project section

---
+++ src/components/webService/WebProjects.jsx
@@ -0,0 +1,62 @@
+const WebProjects = () => {
+    return (
+        <section className='mt-20'>
+            <div className='max-w-[1280px] flex flex-col gap-[100px] mx-auto p-4' >
+            <h2 className="text-6xl font-bold">Our Projects</h2>
+                {/* Project - 1 */}
+                <div className='flex flex-col-reverse rounded-md md:flex-row-reverse lg:flex-row p-3 lg:p-10 shadow-2xl' >
+                    <div className='py-8 pl-7 lg:px-10' >
+                        <div className="w-4/5">  
+                            <h2 className=' text-4xl xl:text-6xl  font-[500] mb-8' > <span className="text-green-500">01.</span> Share tools quickly and confidently in minutes</h2>
+                            <p>This powerful tool eliminates the need to leave Salesforce to get things done as I can create a custom proposal with dynamic pricing tables. You can also customize your own dynamic versions.</p>
+                        </div>
+                    </div>
+                    <div className='w-full lg:min-w-[500px] max-w-[640px] rounded-4xl overflow-clip '>
+                        <img src="/images/feature-01.png" alt="img" />
+                    </div>
+                </div>
+               
+                <div className='flex flex-col-reverse rounded-md md:flex-row-reverse lg:flex-row p-3 lg:p-10 shadow-2xl' >
+                    <div className='py-8 pl-7 lg:px-10' >
+                        <div className="w-4/5">  
+                            <h2 className=' text-4xl xl:text-6xl  font-[500] mb-8' ><span className="text-green-500">02.</span>Share tools quickly and confidently in minutes</h2>
+                            <p>This powerful tool eliminates the need to leave Salesforce to get things done as I can create a custom proposal with dynamic pricing tables. You can also customize your own dynamic versions.</p>
+                        </div>
+                    </div>
+                    <div className='w-full lg:min-w-[500px] max-w-[640px] rounded-4xl overflow-clip '>
+                        <img src="/images/feature-01.png" alt="img" />
+                    </div>
+                </div>
+
+
+                 <div className='flex flex-col-reverse rounded-md md:flex-row-reverse lg:flex-row p-3 lg:p-10 shadow-2xl' >
+                    <div className='py-8 pl-7 lg:px-10' >
+                        <div className="w-4/5">  
+                            <h2 className=' text-4xl xl:text-6xl  font-[500] mb-8' ><span className="text-green-500">03.</span>Share tools quickly and confidently in minutes</h2>
+                            <p>This powerful tool eliminates the need to leave Salesforce to get things done as I can create a custom proposal with dynamic pricing tables. You can also customize your own dynamic versions.</p>
+                        </div>
+                    </div>
+                    <div className='w-full lg:min-w-[500px] max-w-[640px] rounded-4xl overflow-clip '>
+                        <img src="/images/feature-01.png" alt="img" />
+                    </div>
+                </div>
+
+
+                 <div className='flex flex-col-reverse rounded-md md:flex-row-reverse lg:flex-row p-3 lg:p-10 shadow-2xl' >
+                    <div className='py-8 pl-7 lg:px-10' >
+                        <div className="w-4/5">  
+                            <h2 className=' text-4xl xl:text-6xl  font-[500] mb-8' ><span className="text-green-500">04.</span>Share tools quickly and confidently in minutes</h2>
+                            <p>This powerful tool eliminates the need to leave Salesforce to get things done as I can create a custom proposal with dynamic pricing tables. You can also customize your own dynamic versions.</p>
+                        </div>
+                    </div>
+                    <div className='w-full lg:min-w-[500px] max-w-[640px] rounded-4xl overflow-clip '>
+                        <img src="/images/feature-01.png" alt="img" />
+                    </div>
+                </div>
+
+            </div>
+        </section>
+    )
+}
+
+export default WebProjects
