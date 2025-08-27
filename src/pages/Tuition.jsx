4bf71fce5ed34478f92ab54f8b0b0354dae190f6
Gurpreet-Singh-01 <gurpreet.singh.work.29@gmail.com>
2025-08-27 18:38:27 +0530

    Added Tuition courses section

--- src/pages/Tuition.jsx
+++ src/pages/Tuition.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import TuitionHero from '../components/tuition/TuitionHero'
+import TuitionCourses from '../components/tuition/TuitionCourses'
 
 const Tuition = () => {
   return (
     <div className='mt-32 md:mt-49'>
         <TuitionHero/>
+        <TuitionCourses/>
     </div>
   )
 }
