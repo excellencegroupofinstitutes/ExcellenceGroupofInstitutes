13fba169e84ec17718fcec6852e19f6db4db0727
Pritha Rai <pritharai873@gmail.com>
2025-08-28 22:48:01 +0530

    FAQ added

--- src/pages/WebService.jsx
+++ src/pages/WebService.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import HeroWebService from '../components/webService/HeroWebService'
 import OurServices from '../components/webService/OurServices'
+import FAQ from '../components/FAQ'
 
 const WebService = () => {
   return (
     <div className='mt-45 md:mt-65'>
         <HeroWebService />
         <OurServices />
+        <FAQ/>
     </div>
   )
 }
