5509eb8f4d178657a9129e8a9d3c4e27d66a682a
Gurpreet-Singh-01 <gurpreet.singh.work.29@gmail.com>
2025-09-05 07:58:22 +0530

    Linked Form with co-curricular activities

--- src/pages/Tuition.jsx
+++ src/pages/Tuition.jsx
@@ -27,7 +27,9 @@ const Tuition = () => {
         <GalleryMarquee/>
         <Locations/>
         <AboutTuitions/>
+        <div id='tuition-demo-form' >
         <BookDemoForm demoReason="Tuition"/>  
+        </div>
     </div>
   )
 }
